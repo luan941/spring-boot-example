c0e02ebd8c38c923b80ad6cca772eef32e02670b
Thanh Luan <chiluan941@gmail.com>
2022-04-04 14:46:40 +0700

    Initialize

---
+++ src/main/java/vn/example/SpringBootExampleApplication.java
@@ -0,0 +1,32 @@
+package vn.example;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public Message hello() {
+		return Message.builder()
+				.message("Hello World!")
+				.code(200)
+				.build();
+	}
+
+	@Data
+	@Builder
+	public static class Message {
+		private String message;
+		private int code;
+	}
+}
